744aa76c9ca93c1ca4475fb8349e0ba5247c18b9
Abhijeet Matkar <120715507+abhijeetmatkar@users.noreply.github.com>
2023-02-01 15:24:19 +0530

    Update AIML Project .py
    Testing Pull

--- AIML Project .py
+++ AIML Project .py
@@ -7,18 +7,3 @@ model = LinearRegression()
 model.fit(X,y)
 model.predict([[3]])
 model.predict([[5]])
-"""
-ML project -  ML Project Flow
-1. Loading dataset
-2. Identify X(independant variable/s),y(Output)
-3. Data analysis[EDA:Exploratory Data Analysis]
-4. Feature selection process: select important feature amongs all
-5. Feature Engineering: creating a new features/columns from available one
-6. Splitting Data into Training segment and Testing segment(OPTIONAL)
-7. Model Training
-8. Model Testing
-9. Performance checking using metrics
-10. Evaluate your performance and try to improve if needed 
-using Hyperparamter tuning
-"""
-
