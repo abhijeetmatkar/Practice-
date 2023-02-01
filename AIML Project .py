ca2611444f1b2140bc0f2641e02f5e8d8ae2d1ca
ARM <abhijeetmatakar@gmail.com>
2023-02-01 15:21:24 +0530

    final Code

--- AIML Project .py
+++ AIML Project .py
@@ -1,61 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 ML Introduction: Linear Regression Algorithm
-# Simple Linear regresssion 1 Input + 1 Output
-
-
-# In[1]:
-
-
-# Lets have a data first
-# In ML, input must be in 2D format
+ In ML, input must be in 2D format
 X = [[1],[2],[3],[4],[5]]
 y = [3,4,2,4,5]
-
-
-# In[2]:
-
-
-# Import LinearRegression class
 from sklearn.linear_model import LinearRegression
-
-
-# In[3]:
-
-
-# Create an object of LR
 model = LinearRegression()
-
-
-# In[5]:
-
-
-# Model Training
-# fit method is used to perform training of algorithm
 model.fit(X,y)
-
-
-# In[6]:
-
-
-# Model Prediction
-# use predict method to get pedcition using new input
 model.predict([[3]])
-
-
-# In[7]:
-
-
 model.predict([[5]])
-
-
-# In[ ]:
-
-
 """
 ML project -  ML Project Flow
 1. Loading dataset
