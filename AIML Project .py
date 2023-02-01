d9f85482042e3ab68b715ae8cf96bc4b5deac51b
ARM <abhijeetmatakar@gmail.com>
2023-02-01 15:26:58 +0530

    Final Check Push

--- AIML Project .py
+++ AIML Project .py
@@ -1,9 +1,1 @@
-ML Introduction: Linear Regression Algorithm
- In ML, input must be in 2D format
-X = [[1],[2],[3],[4],[5]]
-y = [3,4,2,4,5]
-from sklearn.linear_model import LinearRegression
-model = LinearRegression()
-model.fit(X,y)
-model.predict([[3]])
-model.predict([[5]])
+# Finally Done
